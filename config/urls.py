59e584cbde61be5542679de7eb8e61f92999d43b
cdspacenoob <cdspacenoob@gmail.com>
2022-01-19 16:08:49 +0900

    create urls views

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from user.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pushup/', Base.as_view(), name='base'),
+    path('graph/', Graph.as_view(), name='graph'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+# 횟수, 시간 등의 정보를 보내준다
+class Base(APIView):
+    # For APP / username 또는 id에 의한 요청
+    def get(self, request):
+        pass
+
+    # For HW / 기록 등록에 관한 요청
+    def post(self, request):
+        pass
+
+
+# 앱 상에 그래프를 표시하기 위해 전달할 데이터
+class Graph(APIView):
+    def get(self, request):
+        pass
